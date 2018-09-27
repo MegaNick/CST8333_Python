29625eaa8e5bb032fcec8fdcd27ee878afce76b9
MegaNick <meln0025@algonquinlive.com>
2018-09-26 22:25:29 -0400

    Just startup

---
+++ MAJOR.py
@@ -0,0 +1,23 @@
+#To the Glory of God!
+#CST8333 Project by Nikolay Melnik
+
+class Tuna():
+    def __init__(self, REF_DATE=0, GEO="", DGUID="", Food="", categories="", Commodity="", UOM="", UOM_ID="",
+                 SCALAR_FACTOR="", SCALAR_ID="", VECTOR="", COORDINATE="", VALUE=0, STATUS="", SYMBOL="", TERMINATED="",DECIMALS=""):
+        self.REF_DATE = REF_DATE
+        self.GEO = GEO
+        self.DGUID = DGUID
+        self.Food = Food
+        self.categories = categories
+        self.Commodity = Commodity
+        self.UOM = UOM
+        self.UOM_ID = UOM_ID
+        self.SCALAR_FACTOR = SCALAR_FACTOR
+        self.SCALAR_ID = SCALAR_ID
+        self.VECTOR = VECTOR
+        self.COORDINATE = COORDINATE
+        self.VALUE = VALUE
+        self.STATUS = STATUS
+        self.SYMBOL = SYMBOL
+        self.TERMINATED = TERMINATED
+        self.DECIMALS = DECIMALS
